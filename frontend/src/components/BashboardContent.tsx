5c97dc979df623500b637056b9d39acb5de1c66a
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2024-10-11 07:39:03 -0600

    Create dashboard content

---
+++ frontend/src/components/BashboardContent.tsx
@@ -0,0 +1,13 @@
+// MainContent.tsx
+import React from 'react';
+
+const MainContent: React.FC = () => {
+  return (
+    <div>
+      <h2>Welcome to Your Dashboard</h2>
+      <p>Here is content </p>
+    </div>
+  );
+};
+
+export default MainContent;
