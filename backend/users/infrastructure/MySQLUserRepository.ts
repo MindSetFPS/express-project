47fe0d38b0a03b4961730b7f6a3d0ffe6aacdd18
Daniel Escobar <daanielescobar@gmail.com>
2024-10-23 22:07:29 -0600

    databas

--- backend/users/infrastructure/MySQLUserRepository.ts
+++ backend/users/infrastructure/MySQLUserRepository.ts
@@ -1,69 +1,118 @@
-import mySQLRepository from "../../shared/database";
-import UserRepository from "../domain/UserRepository"
+import mySQLRepository from "../../shared/MySQLConnectOptions";
+import UserRepository from "../domain/IUserRepository"
 import User from "../domain/User";
-import { Connection } from "mysql2";
+import mysql, { Connection, ConnectionOptions, createConnection, ResultSetHeader, RowDataPacket } from "mysql2/promise";
+import IMySQLUserRepository from "./IMySQLUserRepository";
+import connectOptions from "../../shared/MySQLConnectOptions";
+// import conn from "./mysql";
 
-class MySQLUserRepository implements UserRepository {
-    mySqlRepository: Connection;
+interface DBUser extends RowDataPacket {
+    0: number,
+    1: string,
+    2: string,
+    3: string
+}
 
-    constructor(mySqlRepository: Connection) {
-        this.mySqlRepository = mySqlRepository;
+class MySQLUserRepository {
+    public conn!: Connection; // add ! so the linter doesnt complain when we dont initialize var in constructor
+    public credentials: ConnectionOptions;
+
+    constructor(credentials: ConnectionOptions) {
+        this.credentials = credentials;
+        this.init()
     }
 
-    public createUser(user: User): User {
-        mySQLRepository.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-            [user.name, user.email, user.email],
-            (error, results) => {
-                if (error) throw error;
-
-                console.log(results)
-            })
-        return user;
+    async init(): Promise<void> {
+        this.conn = await createConnection(this.credentials)
     }
 
-    deleteUser(user: User): void {
-        mySQLRepository.query(
-            'DELETE FROM user WHERE id = ?', // query
-            [user.id], // data
-            (error, results) => { //callback
-                if(error) throw error;
-                console.log(results)
-            })
-    }
-
-    public async getUserByID(id: number): Promise<User | null> {
-        var newuser: User;
-        let query: string = 'SELECT * FROM users WHERE id = ?'
-        let params = [id]
-        
+    async createUser(user: User) {
         try {
-            const results = mySQLRepository.query(query, params)
-            newuser = new User(results[0].name, results[0].email, results[0].password, results[0].id)
-            return newuser;
+            const [result, fields] = await this.conn.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+                [user.name, user.email, user.email],
+            )
+            console.log("result:", result)
+            console.log("fields: ", fields)
+        } catch (erro) {
+            console.error(erro)
+        }
+    }
+
+    /* 
+        deleteUser(user: User): void {
+            this.mySQLRepository.query(
+                'DELETE FROM user WHERE id = ?', // query
+                [user.id], // data
+                (error, results) => { //callback
+                    if(error) throw error;
+                    console.log(results)
+                })
+        }
+      */
+
+    /* getUsers(callback: Function){
+         this.conn.query(
+            'SELECT * FROM users',
+            (_error, rows) => {
+                return callback(rows)
+            }
+        )
+    } */
+
+    async getAllUsers() {
+        try {
+            const [fields] = await this.conn.query('SELECT * FROM users')
+            return fields;
         } catch (error) {
             console.error(error)
         }
-        return null;
+    }
+
+    async getUserByID(id: number): Promise<User> {
+        var newuser: User;
+        let query: string = 'SELECT * FROM users WHERE id = ?;'
+        let params = [id]
+
+        try {
+            const [users] = await this.conn.query<DBUser[]>(query, params)
+
+            users.forEach((user: DBUser) => {
+                console.log(user)
+            })
+            // newuser = new User(results[0][0].name, results[0].email, results[0].password, results[0].id)
+        } catch (error) {
+            console.error(error)
+        }
+        return new User('', '', '', 4);
+        // return null;
     }
 
     // pass the user instance where changes have been applied
-    public async updateUser(user: User): Promise<User | null> {
+    /* public async updateUser(user: User): Promise<User | null> {
         var user: User;
         let query: string = 'UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?'
         let params = [user.name, user.email, user.password, user.id]
         var updatedUser: User;
         
         try {
-            const results = mySQLRepository.query(query, params)
-            updatedUser = new User(results[0].name, results[0].email, results[0].password, results[0].id)
+            const results = await this.mySQLRepository.query(query, params)
+            console.log(results)
+            updatedUser = new User('', '', '', 48)
+            // updatedUser = new User(results[0].name, results[0].email, results[0].password, results[0].id)
             return updatedUser;
         } catch(error){
             console.error(error)
         }
         return null;
-    }
+    } */
 }
 
-let mySqlUserRepository: MySQLUserRepository = new MySQLUserRepository(mySQLRepository)
+let mySqlUserRepository: MySQLUserRepository = new MySQLUserRepository({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'app',
+    rowsAsArray: true,
+})
 
 export default mySqlUserRepository;
