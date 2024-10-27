d8c1d361efbc74a05ddd8a9d76b7250651352ed0
Daniel Escobar <daanielescobar@gmail.com>
2024-10-26 23:40:49 -0600

    configure login

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,19 +1,22 @@
-import { Button, ButtonText } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
-import { View } from "react-native";
-import { router } from "expo-router";
+import { Box } from '@/components/ui/box';
+import { HStack } from '@/components/ui/hstack';
+import { Image } from '@/components/ui/image';
+import { ImageBackground } from '@/components/ui/image-background';
 
-export default function Index(){
-    return(
-        <View className="container mx-auto px-1">
-            <Heading> Inicia sesión </Heading>
-            <Button onPress={() => {
-                router.push("/(tabs)/")
-            }}>
-                <ButtonText >
-                    Botón
-                </ButtonText>
-            </Button>
-        </View>
+import Login from '@/components/Login';
+
+// import myImage from '@/assets/images/login.png'
+
+
+export default function Index() {
+    return (
+        <Box className='flex flex-row'>
+
+            <Image source={require('@/assets/images/login.png')}
+                className='w-9/12 h-screen'
+            >
+            </Image>
+            <Login />
+        </Box>
     )
 }
---
+++ app/components/Login.tsx
@@ -0,0 +1,95 @@
+import { Box } from "./ui/box";
+import { EyeOff, CheckIcon, DiffIcon } from 'lucide-react-native';
+import { Heading } from "./ui/heading";
+
+import { Button, ButtonIcon, ButtonText } from "@/components/ui/button";
+import { Card } from '@/components/ui/card';
+import { Input, InputIcon, InputSlot } from "./ui/input";
+import { Mail } from 'lucide-react-native';
+import { InputField } from "./ui/input";
+import { Checkbox, CheckboxGroup, CheckboxIcon, CheckboxIndicator, CheckboxLabel } from "./ui/checkbox";
+import { Text } from '@/components/ui/text';
+import { HStack } from '@/components/ui/hstack';
+import { Divider } from '@/components/ui/divider';
+
+import GoogleLogo from '../assets/svg/google.svg';
+import { VStack } from "./ui/vstack";
+import { Link, LinkText } from '@/components/ui/link';
+import { router } from "expo-router";
+
+
+
+export default function Login() {
+    return (
+        <Box className="w-3/12 mx-auto flex flex-col justify-center px-16">
+            <Card variant='outline' className='shadow-soft-2' >
+                <VStack space="lg">
+                    <Heading size="5xl"> Inicia sesión </Heading> 
+
+                    <Input
+                        variant="outline"
+                        size="lg"
+                        isDisabled={false}
+                        isInvalid={false}
+                        isReadOnly={false}
+                    >
+                        <InputField placeholder="correo" />
+                        <InputSlot className="pr-3">
+                            <InputIcon as={Mail} /> {/* this is buggy and looks ugly af */}
+                        </InputSlot>
+                    </Input>
+
+
+                    <Input
+                        variant="outline"
+                        size="lg"
+                        isDisabled={false}
+                        isInvalid={false}
+                        isReadOnly={false}
+                    >
+                        <InputField placeholder="contraseña" type="password" />
+                        <InputSlot className="pr-3">
+                            <InputIcon as={EyeOff} />
+                        </InputSlot>
+                    </Input>
+
+                    <HStack className="justify-between" space="4xl">
+                        <Checkbox size="md" isInvalid={false} isDisabled={false}   >
+                            <CheckboxIndicator>
+                                <CheckboxIcon as={CheckIcon} />
+                            </CheckboxIndicator>
+                            <CheckboxLabel>Recuerdame</CheckboxLabel>
+                        </Checkbox>
+
+                        <Text>Olvidaste tu contraseña?</Text>
+                    </HStack>
+
+                    <Button onPress={() => {
+                        console.log("loging in")
+                        router.push('(tabs)')
+                    }}>
+                        <ButtonText >
+                            iniciar sesión
+                        </ButtonText>
+                    </Button>
+
+                    <Box className="flex flex-row justify-center items-center">
+                        <Divider className="w-[15%]"/>
+                        <Text className="ml-12 mr-12">o</Text>
+                        <Divider className="w-[15%] "/>
+                    </Box>
+
+                    <Button variant="outline" size="xl">
+                        <ButtonIcon as={GoogleLogo} size="sm" />
+                        <ButtonText size="lg">
+                            inicia sesión con google
+                        </ButtonText>
+                    </Button>
+                    
+                    <Text>¿No tienes cuenta? <LinkText className="text-black font-bold" onPress={() => {}}>Registrate</LinkText></Text>
+
+                </VStack>
+            </Card>
+        </Box>
+    )
+}
