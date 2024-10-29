377ce6f1e38c7e0194e4ba235448f34d7dddb0a0
Daniel Escobar <daanielescobar@gmail.com>
2024-10-28 18:34:39 -0600

    fix navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,29 +1,27 @@
 import React from "react";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
-import { Button } from "flowbite-react";
+import { Button, Navbar as Nav } from "flowbite-react";
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="z-10 bg-white py-2">
-            <div className="flex justify-between mx-2 bg-blue items-center" >
+        <Nav fluid className="z-10 bg-white py-2">
+            <Nav.Brand>
                 <Logo />
+            </Nav.Brand>
+            <div className="flex">
+                <Nav.Toggle />
+            </div>
+            <Nav.Collapse className="md:flex md:justify-between mx-2 bg-blue items-center" >
                 <div >
                     <SearchBar />
                 </div>
-                <div className="flex">
-                    <Button className="mr-2" pill >
-                        Iniciar sesión
-                    </Button>
-                    <Button className="" pill color="gray">
-                        Registrarme
-                    </Button>
+                <div className="flex py-2 md:py-0 ">
+                    <Button className="mr-2 w-full md:w-auto" > Iniciar sesión</Button>
+                    <Button className="w-full md:w-auto" color="gray">Registrarme</Button>
                 </div>
-
-                {/* <loginButton /> */}
-                {/* <createAccount /> */}
-            </div>
-        </nav>
+            </Nav.Collapse>
+        </Nav>
     );
 }
 
