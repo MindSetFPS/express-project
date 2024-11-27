cf4b75aa719d0c4bd68e19ee5d398ea8886e6727
Daniel Escobar <daanielescobar@gmail.com>
2024-11-26 18:25:59 -0600

    test but not passing lol

--- backend/__tests__/integration/users.spec.ts
+++ backend/__tests__/integration/users.spec.ts
@@ -2,7 +2,7 @@ import app from "../../presentation/express/app";
 import { test, describe, vi } from "vitest";
 import request from "supertest"
 
-describe("GET /users/all", () => {
+describe("integration for users", () => {
 
     vi.stubEnv("MYSQL_HOST", "127.0.0.1")
     vi.stubEnv("MYSQL_USER", "root")
@@ -10,9 +10,26 @@ describe("GET /users/all", () => {
     vi.stubEnv("PORT", "3307")
     vi.stubEnv("MYSQL_DATABASE", "app")
 
-    test('should return 200', async () => {
+    test('should return a users list', async () => {
         await request(app)
         .get("/api/users/all")
         .expect(200)
+    })   
+    
+    test('should return a user', async () => {
+        await request(app)
+        .post("/api/users/create")
+        .send({name: 'john'})
+        .set('Accept', 'application/json')
+        .expect(200)
     })
+    
+    test("should return a user by id", async () => {
+        await request(app)
+       .get("/api/users/1")
+       .set('Accept', 'application/json')
+       .expect(200)
+    })
+
+
 })
--- backend/presentation/express/app.ts
+++ backend/presentation/express/app.ts
@@ -1,6 +1,9 @@
-import 'dotenv/config' // Must go first
-console.log(process.env.MYSQL_HOST)
-console.log(process.env.MYSQL_PASSWORD)
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: `.env.${process.env.ENV}`
+})
+
 import express, { Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { apiRoutes } from './apiRoutes';
@@ -9,6 +12,12 @@ import swaggerOutput from "./swagger_output.json";
 import path from 'path';
 import multer from 'multer';
 import * as Minio from "minio"
+
+const app = express();
+
+if(process.env.ENV === 'dev'){
+    app.use(cors())
+} 
 
 const upload = multer({
     storage: multer.memoryStorage(),
@@ -21,13 +30,6 @@ const minioClient = new Minio.Client({
      accessKey: "193G0yx48FCB1mvuZPaY",
      secretKey: "YR8I1Lx4QKOZeBoRDyUs3nIlsCo9Vjko5r96TIYj"
 });
-
-const app = express();
-
-if(process.env.ENV === 'dev'){
-    console.log("using dev env")
-    app.use(cors())
-}
 
 const port = process.env.PORT || 3000;
 
