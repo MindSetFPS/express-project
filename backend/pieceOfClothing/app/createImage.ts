fe9ecb870a61b09b3ddf50fc4e85794dfcee1813
Daniel Escobar <daanielescobar@gmail.com>
2024-11-27 01:03:08 -0600

    typo

--- backend/pieceOfClothing/app/createImage.ts
+++ backend/pieceOfClothing/app/createImage.ts
@@ -3,5 +3,5 @@ import minioClient from '../../shared/Minio';
 // Here we are not following ddd
 export default function createImage(originalname: string, buffer: Buffer): string {
     minioClient.putObject("app", originalname, buffer)
-    return "htt://localhost:3482/app/" + originalname;
+    return "http://localhost:3482/app/" + originalname;
 }
