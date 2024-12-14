35f8887c9c0f4833c8bdaaec87abdd74a54aa82b
Daniel Escobar <daanielescobar@gmail.com>
2024-12-13 19:07:00 -0600

    temp minio url

--- backend/pieceOfClothing/app/createImage.ts
+++ backend/pieceOfClothing/app/createImage.ts
@@ -3,5 +3,5 @@ import minioClient from '../../shared/Minio';
 // Here we are not following ddd
 export default function createImage(originalname: string, buffer: Buffer): string {
     minioClient.putObject("app", originalname, buffer)
-    return "http://localhost:9000/app/" + originalname;
+    return "http://192.168.1.140:9000/app/" + originalname;
 }
