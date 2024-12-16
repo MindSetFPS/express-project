e7d7aba3ad961566e7114763e1150a0322cc3df8
Daniel Escobar <daanielescobar@gmail.com>
2024-12-16 02:32:06 -0600

    fix

--- app/components/drag/ImageViewer.tsx
+++ app/components/drag/ImageViewer.tsx
@@ -1,16 +1,10 @@
 import { StyleSheet, View } from "react-native";
-import { Image, type ImageSource } from "expo-image";
 
 type Props = {
-    imgSource: ImageSource;
     selectedImage?: string;
 };
 
-export default function OutfitCanvas({ imgSource, selectedImage }: Props) {
-    // const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
-
-    // return <Image source={imageSource} style={styles.image} />;
-
+export default function OutfitCanvas({ selectedImage }: Props) {
     return <View className="w-scren" style={styles.image} />
 }
 
